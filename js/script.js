c52ac25a7d213346f74d5155eb528dfaee0881e4
Ewelina Krysiak <ewe.krysiak@gmail.com>
2019-06-21 12:44:46 +0200

    Rozpoczęte zadanie dodatkowe - do poprawy kalkulator sumy

--- js/script.js
+++ js/script.js
@@ -59,10 +59,10 @@ var index = 0;
 
 function changeSlide(onClick) {
     switch (onClick) {
-        case 'rightg':
+        case 'right':
             sliderDiv[index].classList.remove('visible');
             index++
-            if (index == sliderDIV.length)
+            if (index == sliderDiv.length)
                 index=0
             sliderDiv[index].classList.add("visible");
             break;
@@ -75,21 +75,130 @@ function changeSlide(onClick) {
             break;
 
     }
-
 }
 
-arrowRight.addEventListener("click",function () {
+arrowRight.addEventListener("click",function (event) {
+    event.preventDefault()
     changeSlide('right')
 });
 
-arrowLeft.addEventListener("click",function () {
+arrowLeft.addEventListener("click",function (event) {
+    event.preventDefault()
     changeSlide('left')
 });
 
+// !-------------DROP DOWN
 
 
 
+var arrowKind = document.querySelector('span.kind');
 
 
+    arrowKind.addEventListener("click", function () {
 
+        var listPanelKind = document.querySelector('ul.kind');
+
+        if (listPanelKind.style.display == "none"){
+            listPanelKind.style.display = "block";
+        }else{
+            listPanelKind.style.display = "none";
+        }
+    });
+
+
+var arrowColor = document.querySelector('span.color');
+
+
+arrowColor.addEventListener("click", function () {
+
+    var listPanelColor = document.querySelector('ul.color');
+
+    if (listPanelColor.style.display == "none"){
+        listPanelColor.style.display = "block";
+    }else{
+        listPanelColor.style.display = "none";
+    }
+});
+
+
+var arrowMaterial = document.querySelector('span.material');
+
+
+arrowMaterial.addEventListener("click", function () {
+
+    var listPanelMaterial = document.querySelector('ul.material');
+
+    if (listPanelMaterial.style.display == "none"){
+        listPanelMaterial.style.display = "block";
+    }else{
+        listPanelMaterial.style.display = "none";
+    }
+});
+
+var chairClair = document.querySelector("ul.kind :first-child");
+var chairMargarita = document.querySelector("ul.kind :nth-child(2)");
+var chairSelena = document.querySelector("ul.kind :last-child");
+
+var colorRed = document.querySelector("ul.color :first-child");
+var colorBlack = document.querySelector("ul.color :nth-child(2)");
+var colorOrange = document.querySelector("ul.color :last-child");
+
+var patternTextile = document.querySelector("ul.material :first-child");
+var patternLeather = document.querySelector("ul.material :last-child");
+var transportCheck = document.querySelector("input#transport")
+console.log(transportCheck);
+
+
+var chairValue = document.querySelector("h4.title.value")
+var colorValue = document.querySelector("span.color.value")
+var patternValue = document.querySelector("span.pattern.value")
+var transportValue = document.querySelector("span.transport.value")
+
+
+chairClair.addEventListener("click", function () {
+    document.querySelector("h4.title").innerText = "Chair Clair";
+    chairValue.innerText = "290"
+});
+
+chairMargarita.addEventListener("click", function(){
+    document.querySelector("h4.title").innerText = "Chair Margarita";
+    chairValue.innerText = "330"
+});
+
+chairSelena.addEventListener("click", function(){
+    document.querySelector("h4.title").innerText = "Chair Selena";
+    chairValue.innerText = "350"
+});
+
+colorRed.addEventListener("click", function() {
+    document.querySelector("div.panel_left span.color").innerText = "Czerwony";
+    colorValue.innerText = "0"
+});
+
+colorOrange.addEventListener("click", function(){
+    document.querySelector("div.panel_left span.color").innerText = "Pomarańczowy";
+    colorValue.innerText = "0"
+});
+
+colorBlack.addEventListener("click", function() {
+    document.querySelector("div.panel_left span.color").innerText = "Czarny";
+    colorValue.innerText = "0"
+});
+
+patternTextile.addEventListener("click", function(){
+    document.querySelector("div.panel_left span.pattern").innerText = "Tkanina";
+    patternValue.innerText = "70"
+});
+
+patternLeather.addEventListener("click", function(){
+    document.querySelector("div.panel_left span.pattern").innerText = "Skóra";
+    patternValue.innerText = "200"
+});
+
+
+transportCheck.addEventListener("click", function () {
+    document.querySelector("span.transport").innerText = "Transport"
+
+    transportValue.innerText = "200"
+})
 
